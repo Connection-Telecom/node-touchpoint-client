d9845d044477d3b38f960b72be103ab0bc43b68a
michael <michael@michaeld-VirtualBox.(none)>
2018-08-23 11:35:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+
+# touchpoint-client
+
+Allows you to act as a Touchpoint user from a node server. Currently only text chats are supported.
+
+## Install
+
+    npm install --save touchpoint-client
+
+## Usage
+
+    const touchpoint = require('touchpoint-client');
+    const chat = touchpoint.createChat({customerId: 'tDNRFKYYtyrAnhais', topic: 'test chat'});
+    chat.on('message', msg => {
+      console.log(msg);
+    });
+    chat.on('ready', () => {
+      chat.sendMessage('Hello there!').catch(err => {
+        console.log('Error occurred while sending message');
+      });
+    });
+
+Also see [node-touchpoint-client-sample](https://github.com/Connection-Telecom/node-touchpoint-client-sample).
+
+## API
+
+### touchpoint.isAvailable(customerId[, team[, url]])
+
+Check if a team is available (i.e. has logged-in text agents who are not paused).
+
+#### Arguments
+
+* `customerId` *string required*: ID of the customer to check
+* `team` *string*: Name of the team to check, `default` if unspecified
+* `url` *string*: URL of the Touchpoint server, including the trailing `/`. `https://touchpoint.telviva.com/` if unspecified
+
+#### Returns
+
+Promise which resolves to `{ available: true }` or `{ available: false }`.
+
+### touchpoint.createChat(options)
+
+Create a new chat client.
+
+#### Arguments
+
+* `options.customerId` *string required*: ID of the customer to chat to.
+* `options.team` *string*: Name of the team to chat to, `default` if unspecified
+* `options.topic` *string*: Chat topic.
+* `options.signedContext` *string*: JSON object containing the signed context fields, if necessary
+* `options.signature` *string*: base64-encoded signature of the signed context, if necessary
+* `options.unsignedContext` *string|object*: JSON object containing the unsigned context fields, if necessary
+* `options.url` *string*: URL of the Touchpoint websocket. `wss://touchpoint.telviva.com/websocket` if unspecified
+
+#### Returns
+
+Chat instance, which is also an `EventEmitter`.
+
+### chat.sendMessage(message)
+
+Send a message to the agent. Must be called after the `ready` event is emitted.
+
+#### Arguments
+
+* `message` *string required*: Message to send.
+
+#### Returns
+
+Promise which resolves to `undefined` once the message is sent. If Touchpoint modified the message (e.g. to mask a credit card number),
+the promise will resolve to the new value.
+
+### chat.setUserIsTyping(isTyping)
+
+Set whether or not the user is currently typing a message. Note that sending a message
+automatically sets this to `false`. Must be called after the `ready` event is emitted.
+
+#### Arguments
+
+* `isTyping` *boolean required*: `true` if the user started typing, or `false` if the user stopped typing.
+
+#### Returns
+
+Promise which resolves to `undefined` once the status was set.
+
+### chat.close()
+
+Close the chat and tear down the Websocket connection. Returns nothing.
+
+## Chat events
+
+* `error`: emitted with an `Error` if a fatal error occurs. This also closes the chat.
+* `message`: emitted with a message object sent by the agent or by Touchpoint itself.
+  This will always have two fields `type` and `message`, and may have more.
+  `type` is `agentMsg` for a message sent by the agent; all other types are messages sent by the system.
+* `ready`: emitted with no arguments once the chat has finished initializing.
+* `agentIsTyping`: emitted with `{ agentIsTyping: true }` or `{ agentIsTyping: false }` when the agent starts or
+  stops typing a message.
+* `agentId`: emitted with `{ agentId: string|null }` when the agent taking the chat changes or if the chat enters the queue.
+   If the chat enters the queue, `agentId` is `null`. This is generally accompanied by a message with type `claim` or `queue`.
+* `closed`: emitted with no arguments if the agent closes the chat. It is not necessary to call `chat.close` yourself in this case.
---
+++ index.js
@@ -0,0 +1,169 @@
+
+'use strict';
+
+const EventEmitter = require('events');
+const WebSocket = require('ws');
+const request = require('request');
+const Asteroid = require('asteroid').createClass();
+
+class TouchpointError extends Error {
+  constructor(obj) {
+    super(obj != null ? obj.message : 'Unknown error');
+    if (obj != null) {
+      this.reason = obj.reason || obj.message;
+      this.errorCode = obj.error;
+    }
+  }
+}
+
+class Chat extends EventEmitter {
+  constructor(options) {
+    super();
+    options = Object.assign({
+      topic: '<no topic>',
+      team: 'default',
+      signedContext: null,
+      signature: null,
+      unsignedContext: null,
+      url: 'wss://touchpoint.telviva.com/websocket'
+    }, options);
+
+    if (typeof options.customerId !== 'string') {
+      throw new Error('customerId must be a string');
+    }
+
+    if (typeof options.unsignedContext === 'object') {
+      options.unsignedContext = JSON.stringify(options.unsignedContext);
+    }
+
+    if (options.signedContext != null && typeof options.signedContext !== 'string') {
+      throw new Error('signedContext must be a string, if present');
+    }
+
+    this._closed = false;
+    this._agentIsTyping = false;
+    this._agentId = null;
+
+    this._ddp = new Asteroid({
+      endpoint: options.url,
+      SocketConstructor: WebSocket,
+      autoReconnect: false
+    });
+
+    this._expectedDisconnect = false;
+    this._ddp.ddp.on('disconnected', () => {
+      if (!this._expectedDisconnect) {
+        this._closed = true;
+        this.emit('error', new Error('unexpected DDP disconnection'));
+      }
+    })
+
+    this._ddp.ddp.on('added', message => {
+      if (message.collection === 'chats') {
+        this._chatUpdated(message.fields);
+      } else if (message.collection === 'messages') {
+        this.emit('message', message.fields);
+      }
+    });
+    this._ddp.ddp.on('changed', message => {
+      if (message.collection === 'chats') {
+        this._chatUpdated(message.fields);
+      }
+    });
+
+    this._ddp.call(
+      'createChat', options.customerId, options.topic, options.team, 'text', options.signedContext, options.signature, options.unsignedContext
+    ).then(id => {
+      this._chatId = id;
+      this._ddp.subscribe('userChat', id).on('ready', () => {
+        this.emit('ready');
+      }).on('error', error => {
+        this.emit('error', error);
+        this.close();
+      });
+
+    }).catch(err => {
+      this.emit('error', err);
+      this.close();
+    });
+  }
+
+  _chatUpdated(chat) {
+    if (chat.hasOwnProperty('agentIsTyping')) {
+      this.emit('agentIsTyping', { agentIsTyping: chat.agentIsTyping });
+    }
+
+    if (chat.hasOwnProperty('agentId')) {
+      this.emit('agentId', { agentId: chat.agentId });
+    }
+
+    if (chat.isClosed) {
+      this.emit('closed');
+      this.close();
+    }
+  }
+
+  sendMessage(message) {
+    if (this._chatId == null) {
+      return Promise.reject(Error('chat has not been created yet'));
+    }
+    if (this._closed) {
+      return Promise.reject(Error('chat was already closed'));
+    }
+    return this._ddp.call('postMessageAsUser', this._chatId, message);
+  }
+
+  setUserIsTyping(userIsTyping) {
+    if (this._chatId == null) {
+      return Promise.reject(Error('chat has not been created yet'));
+    }
+    if (this._closed) {
+      return Promise.reject(Error('chat was already closed'));
+    }
+    return this._ddp.call('setUserIsTyping', this._chatId, userIsTyping);
+  }
+
+  close() {
+    this._expectedDisconnect = true;
+    this._closed = true;
+    this._ddp.disconnect();
+  }
+}
+
+
+module.exports = {
+  createChat: (options) => new Chat(options),
+
+  isAvailable(customerId, team, url) {
+    if (url == null) {
+      url = 'https://touchpoint.telviva.com/';
+    }
+
+    if (team == null) {
+      team = 'default';
+    }
+
+    return new Promise((resolve, reject) => {
+      request({
+        uri: url + 'api/customers/' + encodeURIComponent(customerId) + '/teams/' + encodeURIComponent(team) + '/channels',
+        json: true
+      }, (err, result) => {
+        if (err != null) {
+          return reject(err);
+        }
+
+        if (result.statusCode >= 300) {
+          return reject(new TouchpointError(result.body));
+        }
+
+        for (let channel of result.body) {
+          if (channel.channel === 'text') {
+            return resolve({ available: channel.available });
+          }
+        }
+
+        return resolve({ available: 'false' });
+      });
+    });
+  }
+};
